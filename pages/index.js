3bbb822e133f0ae002bf55e0cacf4e524307905b
Sam <closer.earth@protonmail.com>
2023-11-17 17:12:06 +0000

    Remove lorem ipsum

--- pages/index.js
+++ pages/index.js
@@ -67,11 +67,10 @@ export default function Home() {
       >
         <div className="w-[90%] mx-auto pt-20">
           <p className="title-banner font-urbanist font-[700] text-[35px] md:text-xl text-white">
-            Pioneering the Future <br /> of Regenerative Living <br /> Through
-            Technology.
+            Pioneering the Future <br /> of Regenerative Living <br /> Through Technology.
           </p>
-          <p className="des-banner font-[400] font-urbanist text-[#A5A5A5] text-lg mt-5">
-          Lorem ipsum dolor sit amet consectetur. Risus porttitor orci convallis turpis est leo tellus facilisi et. Consectetur pulvinar euismod purus gravida ferment
+          <p className="font-[400] font-urbanist text-[#A5A5A5] text-lg mt-5">
+            The operating system for regenerative communities.
           </p>
           <div className="mt-5 ">
             <button className="banner-btn py-3 px-6 rounded-xl bg-gradient-to-r from-[#67F8C0] to-[#3F91DD]  font-[700] font-urbanist text-[#171717] text-lg">Get Started</button>
